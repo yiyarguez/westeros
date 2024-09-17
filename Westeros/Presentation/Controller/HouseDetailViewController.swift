9fa587dfa3a43c113c38339e24d9045cf4922557
Kary Rodríguez <yiyarguez@gmail.com>
2024-09-16 19:22:31 -0600

    Crecion de viewController y XIB para mostrar el detalle de la house seleccioanada por el usuario WIP

---
+++ Westeros/Presentation/Controller/HouseDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HouseDetailViewController.swift
+//  Westeros
+//
+//  Created by Kary Rguez on 16/09/24.
+//
+
+import UIKit
+
+final class HouseDetailViewController: UIViewController {
+    @IBOutlet weak var houseImageView: UIImageView!
+    @IBOutlet weak var houseNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+}
